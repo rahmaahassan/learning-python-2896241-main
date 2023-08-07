be71f344367c1777fff466d090f7532a64a9c3d2
rahmaahassan <rahmahassan536@gmail.com>
2023-08-07 11:04:48 +0300

    variables

--- learning-python-2896241-main/Ch2 - Basics/variables_start.py
+++ learning-python-2896241-main/Ch2 - Basics/variables_start.py
@@ -13,25 +13,41 @@ mylist = [0, 1, "two", 3.2, False]
 mytuple = (0, 1, 2)
 mydict = {"one" : 1, "two" : 2}
 
-print(myint)
-print(myfloat)
-print(mystr)
-print(mybool)
-print(mylist)
-print(mytuple)
-print(mydict)
+# print(myint)
+# print(myfloat)
+# print(mystr)
+# print(mybool)
+# print(mylist)
+# print(mytuple)
+# print(mydict)
 
 # re-declaring a variable works
+# myint = 'abc'
+# print(myint)
 
 # to access a member of a sequence type, use []
+# print(mylist[2])
+# print(mytuple[1])
 
 # use slices to get parts of a sequence
+print(mylist[1:5])
 
 # you can use slices to reverse a sequence
+print(mylist[::-1])
 
 # dictionaries are accessed via keys
+# myDic = {"name": 'rahme', "number": 2}
+# print(myDic)
+# print(mydict)
 
 # ERROR: variables of different types cannot be combined
+# print('rahma' + 3)
 
 # Global vs. local variables in functions
+def check():
+    global a 
+    a = 3
+    print(a)
 
+check()
+print(a)
