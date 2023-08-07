dd95f1a2b53bf978fd05375db43bd3bd66581463
rahmaahassan <rahmahassan536@gmail.com>
2023-08-07 10:46:16 +0300

    hello world

--- learning-python-2896241-main/Ch2 - Basics/helloworld_start.py
+++ learning-python-2896241-main/Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,9 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+    name = input('What is your name? ')
+    print(f'Nice to meet you', name)
 
+if __name__ == "__main__":
+    main()
