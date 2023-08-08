03cc40e9a37e0db7b8448d4aca9e7cca6464205b
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-08-08 11:58:42 +0300

    Add files via upload

---
+++ Learning-python/learning-python-2896241-main/Ch2 - Basics/helloworld_start.py
@@ -0,0 +1,11 @@
+#
+# Example file for HelloWorld
+# LinkedIn Learning Python course by Joe Marini
+#
+
+def main():
+    name = input('What is your name? ')
+    print(f'Nice to meet you', name)
+
+if __name__ == "__main__":
+    main()
