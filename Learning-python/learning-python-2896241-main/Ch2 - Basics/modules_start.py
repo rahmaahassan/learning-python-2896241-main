89a502b975bc409822d007fe4717b16aceb74aa3
rahmaahassan <rahmahassan536@gmail.com>
2023-08-13 10:26:21 +0300

    modules - math module

--- Learning-python/learning-python-2896241-main/Ch2 - Basics/modules_start.py
+++ Learning-python/learning-python-2896241-main/Ch2 - Basics/modules_start.py
@@ -3,12 +3,13 @@
 
 
 # TODO: import the math module, which contains features for working with mathematics
-
+import math
 
 # TODO: the math module contains lots of pre-built functions
-
+print('The squre root of 16 is ', math.sqrt(16))
 
 # TODO: in addition to functions, some modules contain useful constants 
-
+print('The Pi is', math.pi)
 
 # TODO: try some of the math functions for yourself here:
+print(math.gamma(20), math.log(16))
