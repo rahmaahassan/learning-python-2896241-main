88c1ade4d5f1502ca98052031008ee19f03f0d3e
rahmaahassan <rahmahassan536@gmail.com>
2023-08-13 10:21:00 +0300

    classes

--- Learning-python/learning-python-2896241-main/Ch2 - Basics/classes_start.py
+++ Learning-python/learning-python-2896241-main/Ch2 - Basics/classes_start.py
@@ -3,3 +3,46 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+class Vehicle():
+    def __init__(self, bodystyle):
+        self.bodystyle = bodystyle
+    
+    def drive(self, speed):
+        self.mode = 'driving'
+        self.speed = speed
+
+class Car(Vehicle):
+    def __init__(self, enginetypr):
+        super().__init__('Car')
+        self.wheels = 4
+        self.doors = 4
+        self.enginetypr = enginetypr
+    
+    def drive(self, speed):
+        super().drive(speed)
+        print('Driving my', self.enginetypr, 'car at', self.speed)
+
+class Motocycle(Vehicle):
+    def __init__(self, enginetypr, hassidecar):
+        super().__init__('Motocycle')
+        if(hassidecar):
+            self.wheels = 3
+        else:
+            self.wheels = 2
+        self.doors = 0
+        self.enginetypr = enginetypr
+    
+    def drive(self, speed):
+        super().drive(speed)
+        print('Driving my', self.enginetypr, 'motocycle at', self.speed)
+
+car1 = Car('gas')
+car2 = Car('electric')     
+mc1 = Motocycle('gas', False)
+
+print(car1.enginetypr)
+print(mc1.wheels)
+print(car2.wheels)
+
+car1.drive(30)
+mc1.drive(50)
